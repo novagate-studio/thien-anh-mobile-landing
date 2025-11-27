54b2469d0d7ece08f125936689402838f95dc87a
Sunday <phamhoangson99@gmail.com>
2025-11-27 17:52:27 +0700

    feat: add header and footer components to article page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,6 +3,8 @@ import ArticleContentBackground from '@/assets/images/article-content-background
 import type { Metadata } from 'next'
 import Image from 'next/image'
 import Content from './components/content'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 type Props = {
   params: Promise<{ slug: string }>
@@ -12,7 +14,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = (await params).slug
 
   // fetch post information
-  const post = await fetch(`https://api.novagate.vn/api/v2/articles/${slug}`).then((res) => res.json().then(data => data.data))
+  const post = await fetch(`https://api.novagate.vn/api/v2/articles/${slug}`).then((res) =>
+    res.json().then((data) => data.data)
+  )
 
   return {
     title: post.title || '404 Not Found',
@@ -23,30 +27,36 @@ export default async function Page({ params }: Props) {
   const slug = (await params).slug
 
   // fetch post information
-  const post = await fetch(`https://api.novagate.vn/api/v2/articles/${slug}`).then((res) => res.json().then(data => data.data))
+  const post = await fetch(`https://api.novagate.vn/api/v2/articles/${slug}`).then((res) =>
+    res.json().then((data) => data.data)
+  )
   return (
-    <div className='relative w-full article'>
-      <Image
-        src={ArticleBackground}
-        alt='Article Background'
-        className='w-full h-screen object-cover hidden lg:block'
-      />
-      <div className='relative w-full max-w-4xl mx-auto lg:-mt-[40vh] p-3'>
+    <div>
+      <Header />
+      <div className='relative w-full article'>
         <Image
-          src={ArticleContentBackground}
-          alt='Article Content Background'
-          className='w-full h-auto absolute hidden lg:block inset-x-0 top-0'
+          src={ArticleBackground}
+          alt='Article Background'
+          className='w-full h-screen object-cover hidden lg:block'
         />
-        <div className='relative px-4 py-2'>
-          <div className='text-black uppercase text-4xl font-bold text-center w-full'>{post?.tags}</div>
-          <div className='mt-10'>
-            <h1 className='font-bold text-center mb-6 text-red-500 border-b border-dashed border-red-500'>
-              {post?.title}
-            </h1>
-            <Content/>
+        <div className='relative w-full max-w-4xl mx-auto lg:-mt-[40vh] p-3'>
+          <Image
+            src={ArticleContentBackground}
+            alt='Article Content Background'
+            className='w-full h-auto absolute hidden lg:block inset-x-0 top-0'
+          />
+          <div className='relative px-4 py-2'>
+            <div className='text-black uppercase text-4xl font-bold text-center w-full'>{post?.tags}</div>
+            <div className='mt-10'>
+              <h1 className='font-bold text-center mb-6 text-red-500 border-b border-dashed border-red-500'>
+                {post?.title}
+              </h1>
+              <Content />
+            </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
